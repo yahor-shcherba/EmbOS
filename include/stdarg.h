81058f3a25ea22a8c517d91888909b9b01d7afbb
Yahor Shcherba <egorshcherba94@gmail.com>
2022-08-23 16:18:37 +0300

    stdarg: replace end to va_end and va-arg to va_arg

--- include/stdarg.h
+++ include/stdarg.h
@@ -4,7 +4,7 @@
 typedef __builtin_va_list va_list;
 
 #define va_start(ap, last_arg)  __builtin_va_start(ap, last_arg)
-#define va-arg(ap, type)        __builtin_va_arg(ap, type)
-#define end(ap)                 __builtin_va_end(ap)
+#define va_arg(ap, type)        __builtin_va_arg(ap, type)
+#define va_end(ap)              __builtin_va_end(ap)
 
 #endif /* NOT _STDARG_H */
