4dc1e1ccf2103304724374ec3a31188567b36df9
Yahor Shcherba <egorshcherba94@gmail.com>
2022-08-23 21:48:40 +0300

    main: added i8259_setup

--- sys/kernel.c
+++ sys/kernel.c
@@ -1,4 +1,5 @@
 #include <cpu.h>
+#include <i8259.h>
 #include <uart.h>
 #include <mem.h>
 #include <stdio.h>
@@ -6,11 +7,13 @@
 void
 main(void)
 {
-  kmem_setup(); /* setup a memory allocater */
-  cpu_setup();  /* setup a gdt and idt */
-  uart_setup(); /* setup a serial port */
+  kmem_setup();   /* setup a memory allocater */
+  i8259_setup();  /* setup a pic (i8259A) controller */
+  cpu_setup();    /* setup a gdt and idt */
+  uart_setup();   /* setup a serial port */
 
   printf("Welcome to the EmbOS...\n");
+  sti();
 
   while (1)
     ;
