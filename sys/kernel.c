fc4a0df7d135629d84b0b817fc305beefdde141f
Yahor Shcherba <egorshcherba94@gmail.com>
2022-08-24 09:35:09 +0300

    kernel: added i8254_setup

--- sys/kernel.c
+++ sys/kernel.c
@@ -1,5 +1,6 @@
 #include <cpu.h>
 #include <i8259.h>
+#include <i8254.h>
 #include <uart.h>
 #include <mem.h>
 #include <stdio.h>
@@ -9,9 +10,10 @@ main(void)
 {
   kmem_setup();   /* setup a memory allocater */
   i8259_setup();  /* setup a pic (i8259A) controller */
+  i8254_setup();  /* setup a pit (i8254) controller */
   cpu_setup();    /* setup a gdt and idt */
   uart_setup();   /* setup a serial port */
-
+ 
   printf("Welcome to the EmbOS...\n");
   sti();
 
