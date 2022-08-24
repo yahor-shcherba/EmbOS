a2f0d0a1056c3a3585939617ce3c6f7cd39b840b
Yahor Shcherba <egorshcherba94@gmail.com>
2022-08-24 09:34:15 +0300

    implemented i8254_setup

---
+++ sys/i8254.c
@@ -0,0 +1,32 @@
+#include <i8254.h>
+#include <i8259.h>
+#include <io.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#define DIV_CEIL(n, d) (((n) + (d) - 1) / (d))
+
+#define I8254_FREQ        1193182
+#define TIMER_FREQ        100
+
+#define PORT_CHANNEL_0    0x40
+#define PORT_MODE         0x43
+
+#define MODE_RATE_GEN     0x04
+#define MODE_BINARY       0x00
+#define MODE_RW_LSB       0x10
+#define MODE_RW_MSB       0x20
+#define MODE_COUNTER_0    0x00
+
+#define INIT_COMMAND \
+  (MODE_BINARY | MODE_RATE_GEN | MODE_RW_LSB | MODE_RW_MSB | MODE_COUNTER_0)
+
+extern void
+i8254_setup(void)
+{
+  uint16_t word = (I8254_FREQ + TIMER_FREQ - 1) / TIMER_FREQ;
+  outb(PORT_MODE, INIT_COMMAND);
+
+  outb(PORT_CHANNEL_0, word & 0xFF);
+  outb(PORT_CHANNEL_0, word >> 8);
+}
